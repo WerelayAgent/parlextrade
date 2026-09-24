4c5ca829bec7cf5a9eea930a3cf9cc8d447446a1
wrunzfire404 <wrunzfire404@users.noreply.github.com>
2026-09-24 20:36:43 +0700

    Fix API proxy ESM warning, update USDC to USDG, set custom description

--- api/proxy.js
+++ api/proxy.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   try {
     const targetUrl = 'https://www.parlaytrade.online' + req.url;
     
